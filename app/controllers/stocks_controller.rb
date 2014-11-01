5a5e4d5886628742fccc621d1cb1becacd70b0ae
Artem Pakk <apakk@me.com>
2014-11-01 17:39:14 +0200

    Option to delete stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -24,6 +24,11 @@ class StocksController < ApplicationController
       render action: :new
     end
   end
+  
+  def destroy
+    Stock.find(params[:id]).destroy
+    redirect_to root_path
+  end
 
   private
 
