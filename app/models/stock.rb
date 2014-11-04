49f47f193260df52f8b89f73e42db5310bd57842
Artem Pakk <apakk@me.com>
2014-11-04 11:42:29 +0200

    Change validations for numerical attributes and adjust form fields validations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,10 @@
 # Stock model
 class Stock < ActiveRecord::Base
   validates :stock_name, presence: true, uniqueness: true
-  validates :price, numericality: { greater_than: 1 }
-  validates :quantity, numericality: { only_integer: true, greater_than: 1 }
-  validates :percentage, numericality: { greater_than: 1 }
-  validates :years, numericality: { only_integer: true, greater_than: 1 }
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+  validates :percentage, numericality: { greater_than: 0 }
+  validates :years, numericality: { only_integer: true, greater_than: 0 }
 
   def stock_values
     period = (0..years)
